e49c54d3c3293c2845f8d4266662e0b664d04133
Reynel Fuertes <reynelfuertes@gmail.com>
2016-10-04 11:44:13 +0800

    4th Commit

--- SimpleAppTestRepo/model.inc.php
+++ SimpleAppTestRepo/model.inc.php
@@ -1,15 +1,12 @@
 <?php
 	class DB {
-
 		public function SELECT($sql) {
 			$resultsToArray = array();
 			
 			$db = new mysqli("localhost", "root", "", "simple_db");
-			
 			if($db->connect_errno > 0){
 				die('Unable to connect to database [' . $db->connect_error . ']');
 			}
-
 			if(!$result = $db->query($sql)){
 				die('There was an error running the query [' . $db->error . ']');
 			}
@@ -20,9 +17,5 @@
 	
 			return $resultsToArray;
 		}
-		
-		
 	}
-
-
 ?>
